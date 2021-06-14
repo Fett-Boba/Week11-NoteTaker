a0d6a1acb68c23f5ab72d4bd8485eb7a4fc11296
Boba Fett <thugfencer@gmail.com>
2021-06-13 23:02:14 -0700

    Get delete working and correct bug in new note add

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,21 +5,24 @@ const path = require('path');
 const dbFilename = path.join(__dirname, '../../db/db.json');
 
 router.get('/notes', async (req, res) => {
-     console.log('In GET /notes');
-     fs.readFile(dbFilename, (err, data) => {
-          if (err) throw err;
-          res.json(JSON.parse(data));
-     });
+     let notes = fs.readFileSync(dbFilename);
+     res.json(JSON.parse(notes));
 });
 
 router.post('/notes', async (req, res) => {
-     // Read database
+     // Read database and create newnote ID by adding one to the highest id in DB
      let notes = fs.readFileSync(dbFilename);
      let note = JSON.parse(notes);
+     let newId = 0;
+     for (var i = 0; i < note.length; i++) {
+         if (note[i].id > newId) {
+              newId = note[i].id;
+         }; 
+     };
+     newId++;
 
-     // Add new note to database
-     let id = note.length + 1;
-     let jsObj = {id: `${id}`, title: `${req.body.title}`, text: `${req.body.text}`};
+     // Write new note to db
+     let jsObj = {id: `${newId}`, title: `${req.body.title}`, text: `${req.body.text}`};
      note.push(jsObj);
      fs.writeFileSync(dbFilename, JSON.stringify(note, null, 2));
 
@@ -29,39 +32,25 @@ router.post('/notes', async (req, res) => {
 });
 
 router.delete('/notes/:id', async (req, res) => {
+     // Get the ID of item to delete, and read the db
      let id = req.params.id;
-     console.log("PARM ID: " + id);
-
-     // Read database
      let notes = fs.readFileSync(dbFilename);
      let note = JSON.parse(notes);
      let newNote = [];
      let j = 0;
 
-     // Detect if note deleted, and do not move to new array if so
-     // for (var i=0; i < note.length; i++) {
-     //      if (i != id) {
-     //           newNote[j] = note[i];
-     //           j++;
-     //      }
-     // };
-
+     // Delete Note: New array written with deleted note omitted
      for (var i=0; i < note.length; i++) {
-          console.log("DB ID: " + note[i].id);
-          if (note[i].id === id) {
+          if (note[i].id !== id) {
                newNote[j] = note[i];
                j++;
           }
      };
 
-     for (var i=0; i< newNote.length; i++) {
-          console.log(newNote[i]);
-     }
-
-     //fs.writeFileSync(dbFilename, JSON.stringify(newNote, null, 2));
-     //let newNotes = fs.readFileSync(dbFilename);
-     //res.json(JSON.parse(newNotes));
-     
+     // Write and display updated results
+     fs.writeFileSync(dbFilename, JSON.stringify(newNote, null, 2));
+     let newNotes = fs.readFileSync(dbFilename);
+     res.json(JSON.parse(newNotes));
 });
 
 module.exports = router;
